8babf4a290d7b65b1d5d6bf4b03ab2827a9333c4
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2022-03-22 22:04:29 +0800

    feat: enable TLA

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+await Promise.resolve();
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -15,6 +17,8 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <input />
 
         <p className={styles.description}>
           Get started by editing{' '}
